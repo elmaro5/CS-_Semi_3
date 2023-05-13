2fd57bb5d2acc3a919eed75a3283c394afc9e346
elmaro5 <63744384+elmaro5@users.noreply.github.com>
2023-05-13 20:42:54 +0300

    Update Task_23.cs
    task

--- Task_23.cs
+++ Task_23.cs
@@ -1,3 +1,4 @@
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 Console.WriteLine("Введите число: ");
 
 Console.WriteLine( Math.Pow(Convert.ToInt32(Console.ReadLine()), 3));
